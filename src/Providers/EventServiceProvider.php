3442a4fe74bc7a265e6fcc8867eed2a372fd4b7f
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-02 16:42:19 +0200

    Add InstallPlugin.php listener and register it in EventServiceProvider.php

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -4,12 +4,16 @@ namespace Latus\BasePlugin\Providers;
 
 use Latus\BasePlugin\Listeners\AddItemsToAdminNav;
 use Latus\BasePlugin\Events\AdminNavDefined;
+use Latus\BasePlugin\Listeners\InstallPlugin;
 
 class EventServiceProvider extends \Illuminate\Events\EventServiceProvider
 {
     protected array $listen = [
         AdminNavDefined::class => [
             AddItemsToAdminNav::class
+        ],
+        'latus.package.installed.' . PluginServiceProvider::PLUGIN_NAME => [
+            InstallPlugin::class,
         ]
     ];
 }
