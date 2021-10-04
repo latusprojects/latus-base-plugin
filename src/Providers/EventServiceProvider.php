52d72e3ce0c75ffe91a49f4d57a1f29252695c07
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-04 10:55:36 +0200

    Remove listeners from 'listen' array in EventServiceProvider.php

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -4,19 +4,12 @@ namespace Latus\BasePlugin\Providers;
 
 use Latus\BasePlugin\Listeners\AddItemsToAdminNav;
 use Latus\BasePlugin\Events\AdminNavDefined;
-use Latus\BasePlugin\Listeners\InstallPlugin;
 
 class EventServiceProvider extends \Illuminate\Foundation\Support\Providers\EventServiceProvider
 {
     protected $listen = [
         AdminNavDefined::class => [
             AddItemsToAdminNav::class
-        ],
-        'latus.package.installed.' . PluginServiceProvider::PLUGIN_NAME => [
-            InstallPlugin::class,
-        ],
-        'latus.package.updated.' . PluginServiceProvider::PLUGIN_NAME => [
-            InstallPlugin::class,
-        ],
+        ]
     ];
 }
