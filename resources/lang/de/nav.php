b925143b78017a651b49d0fef673e8a472fba646
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-13 18:35:33 +0200

    Add nav.php and page.php for locale 'de' localizations

---
+++ resources/lang/de/nav.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+        'dashboard' => 'Dashboards',
+        'dashboard.overview' => 'Übersicht',
+        'dashboard.statistics' => 'Statistiken',
+    ]
+    +
+    [
+        'content' => 'Content & Marketing',
+
+        'content.pages' => 'Seiten',
+        'content.page.index' => 'Übersicht',
+        'content.page.create' => 'Neu',
+
+        'content.posts' => 'Posts',
+        'content.post.index' => 'Übersicht',
+        'content.post.create' => 'Neu',
+
+        'content.events' => 'Events',
+        'content.event.index' => 'Übersicht',
+        'content.event.create' => 'Neu',
+
+        'content.settings' => 'Einstellungen',
+    ]
+    +
+    [
+        'administration' => 'Administration',
+
+        'admin.rolesAndUsers' => 'Rollen & User',
+        'role.index' => 'Übersicht: Rollen',
+        'role.create' => 'Neu: Rolle',
+        'user.index' => 'Übersicht: User',
+        'user.create' => 'Neu: User',
+
+        'admin.settingsAndPackages' => 'Konfiguration, Themes & Plugins',
+        'plugin.index' => 'Plugins',
+        'theme.index' => 'Themes & Design',
+        'setting.index' => 'Konfiguration',
+    ]
+    +
+    [
+        'context.view' => 'View',
+        'context.edit' => 'Edit',
+        'context.delete' => 'Delete'
+    ];
---
+++ resources/lang/de/page.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'table.row.id' => 'ID',
+    'table.row.title' => 'Titel',
+    'table.row.actions' => 'Aktionen',
+    'table.count' => 'Zeige :shown von :total'
+];
