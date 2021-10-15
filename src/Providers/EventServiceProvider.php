8ac61c0d17eef04ee02caa2771e6c87b56b1b55c
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-15 23:15:42 +0200

    Add registering of listeners for events 'IncludesJsAssets' and 'IncludesCssAssets'

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -4,12 +4,22 @@ namespace Latus\BasePlugin\Providers;
 
 use Latus\BasePlugin\Listeners\AddItemsToAdminNav;
 use Latus\BasePlugin\Events\AdminNavDefined;
+use Latus\BasePlugin\Listeners\AttachCssAssets;
+use Latus\BasePlugin\Listeners\AttachJsAssets;
+use Latus\PluginAPI\Events\IncludesCssAssets;
+use Latus\PluginAPI\Events\IncludesJsAssets;
 
 class EventServiceProvider extends \Illuminate\Foundation\Support\Providers\EventServiceProvider
 {
     protected $listen = [
         AdminNavDefined::class => [
             AddItemsToAdminNav::class
+        ],
+        IncludesJsAssets::class => [
+            AttachJsAssets::class
+        ],
+        IncludesCssAssets::class => [
+            AttachCssAssets::class
         ]
     ];
 }
