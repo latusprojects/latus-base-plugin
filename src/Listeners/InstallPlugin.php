7a691c646ec5b9ff75cef1863350caaf4ed72c56
steelvirus <mlatussek@yahoo.de>
2022-03-01 14:58:07 +0100

    Fix invalid method parameter typehint

--- src/Listeners/InstallPlugin.php
+++ src/Listeners/InstallPlugin.php
@@ -6,10 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Latus\BasePlugin\Database\Seeders\PageSeeder;
 use Latus\ComposerPlugins\Events\PackageInstalled;
+use Latus\ComposerPlugins\Events\PackageUpdated;
 
 class InstallPlugin
 {
-    public function handle(PackageInstalled $event)
+    public function handle(PackageInstalled|PackageUpdated $event)
     {
         app(PageSeeder::class)->run();
 
